ef9b5f4a9ae0c5befea6a1a97321e17cf0557d9b
Faiyaaz <faiyaaz.haque90@gmail.com>
2022-12-15 01:56:29 -0500

    uploaded new resume

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import '../Styles/Home.scss'
 import Foot from "../Foot";
-import resume from '../Faiyaaz-Resume.pdf'
+import resume from '../Faiyaaz_Resume.pdf'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faFileAlt } from "@fortawesome/free-regular-svg-icons";
 import { faGithubSquare, faLinkedin, faTwitter } from "@fortawesome/free-brands-svg-icons";
